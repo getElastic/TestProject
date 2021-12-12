303fd020a43ec50089bc22c105ffaa5038875dc4
Sayantini Basak <sayantini.basak@accenture.com>
2021-12-12 16:15:14 +0530

    Playing around with new project

---
+++ src/main/java/com/sapient/XYZecommmarket/business/ProductBusiness.java
@@ -0,0 +1,51 @@
+package com.sapient.XYZecommmarket.business;
+
+import com.sapient.XYZecommmarket.beans.Product;
+import com.sapient.XYZecommmarket.dao.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ProductBusiness {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts(int offset) {
+        return productRepository.getAllProducts(offset);
+    }
+
+    public List<Product> getProductsByBrand(String brandName, int offset) {
+        return productRepository.getProductsByBrand(brandName, offset);
+    }
+
+    public List<Product> getProductsByPriceRange(Double minPrice, Double maxPrice, int offset) {
+        return productRepository.getProductsByPriceRange(minPrice, maxPrice, offset);
+    }
+
+    public Product getProductBySKU(Integer productId) {
+        return productRepository.getProductBySKU(productId);
+    }
+
+    public Integer getProductCountBySeller(Integer sellerId) {
+        return productRepository.getProductCountBySeller(sellerId);
+    }
+
+    public List<Product> getProductsByTypeAndSize(Integer productType, Integer size, int offset) {
+        return productRepository.getProductsByTypeAndSize(productType, size, offset);
+    }
+
+    public List<Product> getProductsByTypeAndColor(Integer productType, String color,  int offset) {
+        return productRepository.getProductsByTypeAndColor(productType, color, offset);
+    }
+
+    public ProductRepository getProductRepository() {
+        return productRepository;
+    }
+
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+}
